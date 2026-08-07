71301d80c8d26fd57be899a0dd9a351fc29d03a8
subhasreeA <2312109@nec.edu.in>
2026-08-07 11:04:10 +0530

    Remove duplicate blood-main folder

--- blood-main/backend/models/Admin.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const adminSchema = new mongoose.Schema({
-  admin_id: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-});
-module.exports = mongoose.model('Admin', adminSchema);
